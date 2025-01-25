8504510590f2784a37f70b3693a06de7939f08bc
BlackNugget <23023898@myrp.edu.sg>
2025-01-25 22:59:17 +0800

    bananananananaananana

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,43 +1,3 @@
-// import { useEffect } from "react"
-// import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
-
-// // importing the components
-// import WorkoutDetails from "../components/WorkoutDetails"
-// import WorkoutForm from "../components/WorkoutForm"
-
-// const Home = () => {
-//   // const [workouts, setWorkouts] = useState(null)
-//   const {workouts, dispatch} = useWorkoutsContext()
-
-//   useEffect(() => {
-
-//     const fetchWorkouts = async () => {
-//       const response = await fetch('/api/workouts')
-//       const json = await response.json()
-
-//       if (response.ok) {
-//         // setWorkouts(json)
-//         dispatch({type: 'SET_WORKOUTS', payload: json})
-//       }
-//     }
-
-//     fetchWorkouts()
-//   }, [dispatch])
-
-//   return (
-//     <div className="home">
-//       <div className="workouts">
-//         {workouts && workouts.map(workout => (
-//           <WorkoutDetails workout={workout} key={workout._id} />
-//         ))}
-//       </div>
-//       <WorkoutForm/>
-//     </div>
-//   )
-// }
-
-// export default Home
-
 import { useState, useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import WorkoutDetails from "../components/WorkoutDetails";
@@ -55,7 +15,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts");
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts`);
       const json = await response.json();
       if (response.ok) {
         dispatch({ type: "SET_WORKOUTS", payload: json });
